d3e0100bdf8656779302690d1a23ffa225d27368
derehaki <derehaki@gmail.com>
2019-12-17 17:59:50 +0100

    gear4music spider set

--- ni_crawler/spiders/gear4music.py
+++ ni_crawler/spiders/gear4music.py
@@ -2,37 +2,40 @@
 import scrapy
 from .. items import ProductItem
 from scrapy.spiders import CrawlSpider
-
-PRODUCT_PAGES = {
-    'https://www.gear4music.de/de/Recording-and-Computer/Native-Instruments-Maschine-Mikro-MK3/2NUC': {
-        'name': 'Maschine Mikro MK3',
-        'id': 'MaschineMikroMK3',
-        'sku': 'MASCHINEMikroMk3',
-    },
-}
+import math
 
 
 class Gear4MusicSpider(CrawlSpider):
+    DOMAIN = 'gear4music.de'
+    START_URL = 'https://www.gear4music.de/de/native-instruments?page=1'
     name = 'gear4music'
-    allowed_domains = ['gear4music.de']
-
-    def start_requests(self):
-        for url in PRODUCT_PAGES.keys():
-            yield scrapy.Request(url, self.parse)
+    allowed_domains = [DOMAIN]
+    start_urls = tuple([START_URL])
 
     def parse(self, response):
-        url = response.url
 
-        item = ProductItem()
+        products = response.css('.g4m-grid-item.product-card')
+        for product in products:
+            url = 'https://www.' + self.DOMAIN + product.xpath('@href').extract_first()
+            item = ProductItem()
+            item['shop'] = 'gear4music'
+            item['country'] = 'DE'
+            item['product_url'] = url
+            item['name'] = product.css('.product-card-title::text').extract()[0].strip()
+            item['price'] = product.css('.product-card-price::text').extract()[0].split()[0]
+            item['currency'] = product.css('.product-card-price::text').extract()[0].split()[1]
+            item['image_url'] = url
 
-        item['shop'] = 'gear4music'
-        item['country'] = 'DE'
-        item['product_url'] = url
-        item['name'] = PRODUCT_PAGES[url]['name']
-        item['sku'] = PRODUCT_PAGES[url]['sku']
-        price_block = response.css('.info-row-price')
-        item['price'] = price_block.css('[itemprop="price"]')[0].xpath('@content')[0].extract()
-        item['currency'] = price_block.css('[itemprop="priceCurrency"]')[0].xpath('@content')[0].extract()
-        item['image_url'] = response.css('[itemprop="image"]')[0].xpath('@src')[0].extract()
+            yield scrapy.Request(url, meta={'item': item}, callback=self.parse_image_url)
+
+        number_of_pages = math.ceil(int(response.css('body > div.single-column-responsive-layout.style-alt > div.container-fluid.plp-page.content.hide-on-search > div > div > div.widget--product-listing > div > div > div.react-product-listing-widget__message--listing-count > p > span::text')[1].extract()) / 45)
+        for page_number in range(2, number_of_pages):
+            link = '{}?page={}'.format(self.START_URL, page_number)
+            yield scrapy.Request(link, self.parse)
+
+    @staticmethod
+    def parse_image_url(response):
+        item = response.meta['item']
+        item['image_url'] = response.css('[itemprop="image"]')[0].xpath('@src').extract()[0]
 
         yield item
