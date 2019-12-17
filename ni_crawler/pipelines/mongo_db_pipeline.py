d11de500ca468243ec13986b41bc266e1f5b070a
derehaki <derehaki@gmail.com>
2019-12-17 15:02:57 +0100

    mongo pipeline fine tune

--- ni_crawler/pipelines/mongo_db_pipeline.py
+++ ni_crawler/pipelines/mongo_db_pipeline.py
@@ -10,17 +10,14 @@ class MongoDBPipeline(object):
             "ni-scraped-products-shard-00-01-njxbf.gcp.mongodb.net:27017,"
             "ni-scraped-products-shard-00-02-njxbf.gcp.mongodb.net:27017/test?ssl=true&replicaSet=ni-scraped-products"
             "-shard-0&authSource=admin&retryWrites=true&w=majority&tlsAllowInvalidCertificates=true")
+        self.database = 'scraped-products'
+        self.collection = 'items'
 
     def process_item(self, item, spider):
-        valid = True
-        for data in item:
-            if not data:
-                valid = False
-                raise DropItem("Missing {0}!".format(data))
-        if valid:
-            collection = self.client.get_database('scraped-products').get_collection('items')
-            item_exists = collection.find_one_and_update({"sku": item['sku'], "shop": item['shop']},
-                                                         {"$set": dict(item)}, upsert=True)
-            print(item_exists)
-            # collection.insert(dict(item))
+        if item:
+            collection = self.client.get_database(self.database).get_collection(self.collection)
+            collection.find_one_and_update({"sku": item['sku'], "shop": item['shop']},
+                                           {"$set": dict(item)}, upsert=True)
+        else:
+            raise DropItem("Missing {0}!".format(item))
         return item
