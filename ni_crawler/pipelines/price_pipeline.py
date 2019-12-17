ece9733f083c95ee8e567afea7fa0e09ea288fdf
derehaki <derehaki@gmail.com>
2019-12-17 16:31:39 +0100

    price pipeline updated

--- ni_crawler/pipelines/price_pipeline.py
+++ ni_crawler/pipelines/price_pipeline.py
@@ -6,7 +6,7 @@ from scrapy.exceptions import DropItem
 class PricePipeline(object):
 
     def process_item(self, item, spider):
-        if item.get('price') and int(float(item.get('price'))) > 0 and item.get('currency'):
+        if item.get('price') and item.get('currency'):
             return item
         else:
             raise DropItem("Missing price in %s" % item)
